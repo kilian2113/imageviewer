5ccd34d129d7932b113cee916cda8020984eb40c
kiliansd21 <kiliansantanadelgado@gmail.com>
2026-01-15 19:46:28 +0000

    implement ImageProvider

---
+++ src/main/java/software/ulpgc/imageviewer/architecture/ImageProvider.java
@@ -0,0 +1,49 @@
+package software.ulpgc.imageviewer.architecture;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class ImageProvider {
+    private final List<String> images;
+
+    private ImageProvider(Stream<String> images) {
+        this.images = images.toList();
+    }
+
+    public static ImageProvider with(Stream<String> images) {
+        return new ImageProvider(images);
+    }
+
+    public Image first(Function<String, byte[]> loader) {
+        return load(0, loader);
+    }
+
+    private Image load(int index, Function<String, byte[]> loader) {
+        return new Image() {
+            private final int size = images.size();
+            private byte[] bitmap;
+
+            @Override
+            public String id() {
+                return images.get(index);
+            }
+
+            @Override
+            public byte[] bitmap() {
+                if (bitmap == null) bitmap = loader.apply(images.get(index));
+                return bitmap;
+            }
+
+            @Override
+            public Image next() {
+                return index == size - 1 ? load(0, loader) : load(index + 1, loader);
+            }
+
+            @Override
+            public Image previous() {
+                return index == 0 ? load(size - 1, loader) : load(index - 1, loader);
+            }
+        };
+    }
+}
